40f9d3fb0f3ad4406e7e793e0521c327529c5496
garayaka <samarasinha.isuru@gmail.com>
2021-07-20 20:55:04 +0530

    Add project files.

---
+++ SwaggerTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SwaggerTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        /// <summary>
+        /// This service calls the name 
+        /// </summary>
+        /// <param name="user">Pass the name here</param>
+        /// <returns></returns>
+        [HttpPost("TestServiceCall")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<UserModel> TestServiceCall(UserModel user)
+        {
+            user.Id = 1542;
+
+            return user;// CreatedAtRoute("Object", new { id = user.Id }, user);
+        }
+    }
+}
---
+++ SwaggerTest/Models/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerTest.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} is required")]
+        public int Age { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public List<Address> Address { get; set; }
+    }
+
+    public class Address
+    {
+        [Required]
+        public AddressType AddressType { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+
+    public enum AddressType
+    {
+        BillTo = 0,
+        ShipTo = 1
+    }
+
+}
